5917b27724e5c6fd76ab136656d9f0b775daf9d2
salangdung-i <dladmswl92@naver.com>
2021-12-16 21:33:54 +0900

    Add main.js

---
+++ main.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const input = document.querySelector('.footer__text');
+const form = document.querySelector('.new-form');
+const items = document.querySelector('.items');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  addText()
+});
+
+function addText() {
+  const text = input.value;
+
+  if (text === '') {
+    input.focus();
+    return;
+  }
+  const item = createItem(text);
+  items.appendChild(item);
+  item.scrollIntoView({ block: 'center' });
+  input.value = '';
+  input.focus();
+}
+
+let id = 0;
+function createItem(text) {
+  const itemRow = document.createElement('li');
+  itemRow.setAttribute('class', 'item__row');
+  itemRow.setAttribute('data-id', id);
+
+  itemRow.innerHTML = `
+        <div class="item">
+          <span class="item__name">${text}</span>
+          <button class="item__delete">
+            <i class="fas fa-trash-alt" data-target-id=${id} ></i >
+          </button >
+        </div >
+    <div class="item__divider"></div>
+  `;
+
+  id++;
+  return itemRow;
+}
+
+items.addEventListener('click', event => {
+  const id = event.target.dataset.targetId;
+  if (id) {
+    const toBeDelete = document.querySelector(`.item__row[data-id="${id}"]`);
+    toBeDelete.remove();
+  }
+});
